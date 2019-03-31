a4a7d4fb3739eb6a7ac408aca88dc45805be5810
roger <rogerokello@gmail.com>
2019-03-31 15:49:05 +0300

    Name URL patterns for easy mappings

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,16 +2,16 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-
 # The HyperlinkedModelSerializer has the following differences from ModelSerializer:
 # - It does not include the id field by default.
 # - It includes a url field, using HyperlinkedIdentityField.
 # - Relationships use HyperlinkedRelatedField, instead of PrimaryKeyRelatedField.
 
+
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     # The source argument controls which attribute is used to populate
     # a field, and can point at any attribute on the serialized instance.
-    # It can also take the dotted notation shown below, in which case 
+    # It can also take the dotted notation shown below, in which case
     # it will traverse the given attributes, in a similar way as it is
     # used with Django's template language.
     # The untyped ReadOnlyField is always read-only, and will be used for
@@ -26,11 +26,13 @@ class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     # Because we've included format suffixed URLs such as '.json', we also
     # need to indicate on the highlight field that any format suffixed
     # hyperlinks it returns should use the '.html' suffix.
-    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        fields = ('url', 'id', 'highlight', 'title', 'code', 'linenos',
+                  'language', 'style', 'owner')
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -42,4 +44,4 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
