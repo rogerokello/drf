cde6bf955f5ade59d817553d2ce951bb7dfdf8cc
roger <rogerokello@gmail.com>
2019-03-31 15:35:15 +0300

    Modify serializers to inherit from hyperlinked model serializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+# The HyperlinkedModelSerializer has the following differences from ModelSerializer:
+# - It does not include the id field by default.
+# - It includes a url field, using HyperlinkedIdentityField.
+# - Relationships use HyperlinkedRelatedField, instead of PrimaryKeyRelatedField.
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     # The source argument controls which attribute is used to populate
     # a field, and can point at any attribute on the serialized instance.
     # It can also take the dotted notation shown below, in which case 
@@ -14,12 +19,21 @@ class SnippetSerializer(serializers.ModelSerializer):
     # instances when they are deserialized.
     owner = serializers.ReadOnlyField(source='owner.username')
 
+    # Notice that we've also added a new 'highlight' field. This field is of
+    # the same type as the url field, except that it points to the
+    # 'snippet-highlight' url pattern, instead of the 'snippet-detail' url
+    # pattern.
+    # Because we've included format suffixed URLs such as '.json', we also
+    # need to indicate on the highlight field that any format suffixed
+    # hyperlinks it returns should use the '.html' suffix.
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     # Because 'snippets' is a reverse relationship on the User model, it will not be included
     # by default when using the ModelSerializer class, so we needed to add an explicit field
     # for it.
