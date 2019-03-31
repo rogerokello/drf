f9e371e211bceb447f3bbf7be755ecbac4fd379e
roger <rogerokello@gmail.com>
2019-03-31 10:36:36 +0300

    Add mixins for common functionality

--- snippets/views.py
+++ snippets/views.py
@@ -19,64 +19,52 @@
 # It's a good idea to use these throughout rather than using numeric identifiers.
 
 # ----------- Wrapping API views --------------
-# The create/retrieve/update/delete operations that we've been using so far are going to be 
+# The create/retrieve/update/delete operations that we've been using so far are going to be
 # pretty similar for any model-backed API views we create. Those bits of common behaviour are
 # implemented in REST framework's mixin classes.
 
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework import generics
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(APIView):
+# Here we're building our view using GenericAPIView, and adding in ListModelMixin and
+# CreateModelMixin. The base class(GenericAPIView) provides the core functionality, and the
+# mixin classes provide the .list() and .create() actions. We're then explicitly binding the
+# get and post methods to the appropriate actions.
+class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin,
+                  generics.GenericAPIView):
     """
     List all code snippets, or create a new snippet.
     """
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class SnippetDetail(APIView):
+# Again we're using the GenericAPIView class to provide the core functionality, and adding
+# in mixins to provide the .retrieve(), .update() and .destroy() actions.
+class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin, generics.GenericAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
 
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    @csrf_exempt
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    @csrf_exempt
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
