b4e3bfd8126ea4c4f938a9164f6faf475ef9d701
roger <rogerokello@gmail.com>
2019-04-01 09:34:41 +0300

    Use routers and remove the api_root from the views module

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,32 +1,27 @@
-from django.urls import path
-from rest_framework import renderers
-from rest_framework.urlpatterns import format_suffix_patterns
-from snippets.views import SnippetViewSet, UserViewSet, api_root
+# Because we're using ViewSet classes rather than View classes,
+# we actually don't need to design the URL conf ourselves. The
+# conventions for wiring up resources into views and urls can be
+# handled automatically, using a Router class. All we need to do
+# is register the appropriate view sets with a router, and let it
+# do the rest.
 
-# Notice how we're creating multiple views from each ViewSet class,
-# by binding the http methods to the required action for each view.
-snippet_list = SnippetViewSet.as_view({'get': 'list', 'post': 'create'})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view(
-    {'get': 'highlight'}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({'get': 'list'})
-user_detail = UserViewSet.as_view({'get': 'retrieve'})
+# Registering the viewsets with the router is similar to providing
+# a urlpattern. We include two arguments - the URL prefix for the
+# views, and the viewset itself.
 
-# Now that we've bound our resources into concrete views,
-# we can register the views with the URL conf as usual.
-urlpatterns = format_suffix_patterns([
-    path('', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path(
-        'snippets/<int:pk>/highlight/',
-        snippet_highlight,
-        name='snippet-highlight'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail')
-])
+# The DefaultRouter class we're using also automatically creates the
+# API root view for us, so we can now delete the api_root method from
+# our views module.
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
